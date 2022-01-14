46df37e3f4fb7821baedce5b279298c385f0b5a4
Anik Dhar <aniksutradhar11@gmail.com>
2022-01-14 16:52:00 +0600

    responsive done

--- src/Components/Home/HomeSection-1/HomeSection1.js
+++ src/Components/Home/HomeSection-1/HomeSection1.js
@@ -6,18 +6,18 @@ const HomeSection1 = () => {
         <div className='mt-5 text-start p-4'>
             <Container className='shadow-lg'>
                 <Row>
-                    <Col className='border-end border-2 link p-4'>
+                    <Col sm={4} className='border-end border-2 link p-4'>
                     <h5>See Sage Intacct in action</h5>
                     <p>Learn what Sage Intacct can offer you with a live demo followed by Q&A.</p>
                    <a href="#a">Sign up now <i class="fas fa-arrow-right"></i></a>
                     </Col>
-                    <Col className='border-end border-2 link p-4'>
+                    <Col sm={4} className='border-end border-2 link p-4'>
                     <h5>#1 for the 5th year in a row</h5>
                     <p>Once again, Sage Intacct received the highest product score in core financials for the lower midsize enterprises use case from Gartner_1.</p>
                     <a href="#a">Read report <i class="fas fa-arrow-right"></i></a>
 
                     </Col>
-                    <Col className='link p-4'>
+                    <Col sm={4} className='link p-4'>
                     <h5>Outgrowing QuickBooks?</h5>
                     <p>Find out why thriving organizations say goodbye to QuickBooks and partner with Sage Intacct to power their growth.</p>
                     <a href="#a">Learn Why <i class="fas fa-arrow-right"></i></a>
